799c10f4a395ba16157aeb3a4299bf008a7f63e8
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-05-17 21:07:15 +0200

    testing for endpoint finised

---
+++ test/api/departments/delete.test.js
@@ -0,0 +1,28 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../../server.js');
+const Department = require('../../../models/department.model');
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+const request = chai.request;
+
+describe('DELETE /api/departments', async () => {
+    before(async () => {
+        const testDepOne = new Department({ _id: '5d9f1140f10a81216cfd4408', name: 'Department #1' });
+        await testDepOne.save();
+    });
+
+
+    it('/:id should delete document and return success', async () => {
+        const res = await request(server).delete('/api/departments/5d9f1140f10a81216cfd4408');
+        expect(res.status).to.be.equal(200);
+        const newDepartment = await Department.findOne({ name: '#Department #1' });
+        expect(newDepartment).to.be.null;
+    });
+
+    after(async () => {
+        await Department.deleteMany();
+    });
+});
